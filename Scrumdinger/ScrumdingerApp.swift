593a13bf0d3e6c6880afa363a94b52e8985e13b1
Gabriel Rossi <gabrieldesouzarossi123456@gmail.com>
2023-05-18 08:55:10 -0300

    18/05/2023

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Gabriel Rossi on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
